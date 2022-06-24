70c8ce7574de48e1b988b397e31d57dff42e8357
cadumeneses <cadumeneses>
2022-06-24 09:24:32 -0300

    style: navbar

--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-nav-bar',
-    templateUrl: 'nav-bar.component.html'
+    templateUrl: 'nav-bar.component.html',
+    styleUrls: ['nav-bar.component.css']
 })
 
 export class NavBarComponent{
