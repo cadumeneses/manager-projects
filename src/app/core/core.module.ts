813e4e9fe47b03665ede4f022b9048e711721694
cadumeneses <cadumeneses>
2022-06-28 09:31:13 -0300

    feat: add footer | style: footer bg

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @NgModule({
     declarations: [
-        NavBarComponent
+        NavBarComponent,
+        FooterComponent
     ],
     imports: [
         RouterModule
     ],
     exports: [
-        NavBarComponent
+        NavBarComponent,
+        FooterComponent
     ]
 })
 
