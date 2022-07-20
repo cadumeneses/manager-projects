2c3d09e72c8b151aad9da4767ee2e2b9408ac06e
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-07-20 10:40:52 -0300

    test: config cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,10 @@
 describe('empty spec', () => {
   it('acesso ao site', () => {
+    cy.viewport(1920,1280)
     cy.visit('http://localhost:4200/projects')
   })
 
-  it('ghjfkgkld',)
+  it('ghjfkgkld', ()=>{
+    
+  })
 })
