2f86ee0bccf2a79ec0a58fdce6a615065d0ab33e
Carlos Eduardo <carloseduardoms646@gmail.com>
2022-07-26 10:30:00 -0300

    feat: aplication test

---
+++ cypress/e2e/create.cy.ts
@@ -0,0 +1,28 @@
+describe('create project',()=>{
+    it('User create project', () => {
+        cy.viewport(1920,1280)
+        cy.visit('https://manager-projects.vercel.app/projects')
+
+        cy.get('a[href="/projects/new"]').click()
+        cy.get('form h2').should('have.text', ' Cadastrar um novo projeto ')
+
+        // var adm ={
+        //     projectName: 'Teste com cypress',
+        //     team: 'barba branca',
+        //     description: 'teste automatizado utilizando a ferramenta Cypress'
+        // }
+
+        // cy.get('input[name="projectName"]').type(adm.projectName)
+        // cy.get('textarea[formControlName="description"]').type(adm.description)
+        // cy.select('option[value="barba branca"]')
+
+        cy.get('#floatingSelectGrid')
+        .then(($options)=>
+            Cypress._.map($options, ($option) => $option.innerText),
+        )
+        .then((list)=> list.filter((s)=> s !== ''))
+        .invoke('sort')
+        .should('deep.equal', ['one piece', 'naruto', 'barbie', 'barba branca', 'senai', 'crato'])
+
+      })
+})
